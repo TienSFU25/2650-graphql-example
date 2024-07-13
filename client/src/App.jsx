57896c41c1b0a56c6328a4f3fd57280e4143de97
Tien Vu <tienv2510@gmail.com>
2024-07-13 17:58:45 -0400

    Attempt to refetch on navigate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useQuery, useApolloClient, gql } from "@apollo/client";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const GetTweets = gql`
   query {
@@ -14,17 +17,74 @@ const GetTweets = gql`
   }
 `;
 
+function Home(props) {
+  const client = useApolloClient();
+  const { id } = useParams();
+  const [tweets, setTweets] = useState([]);
+  console.log(`LOADING APP ID ${id}, props ${JSON.stringify(props)}`);
+  const navigate = useNavigate();
+
+  // const { loading, error, data, refetch } = useQuery(GetTweets, {
+  //   pollInterval: 0,
+  //   notifyOnNetworkStatusChange: true,
+  //   onError: (error) => {
+  //     console.log(`Apollo error: ${JSON.stringify(error, null, 2)}`);
+  //   },
+  //   onCompleted: (data) => {
+  //     console.log(`COMPLETED THE QUERY ${data.length}`);
+  //     // setTweets(data.Tweets);
+  //   },
+  // });
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log(`refetching stuff`);
+      // refetch();
+      const { data } = await client.query({
+        query: GetTweets,
+        fetchPolicy: "network-only",
+      });
+      console.log(`done fetching ${JSON.stringify(data.Tweets)}`);
+      setTweets(data.Tweets);
+    }
+    fetchData();
+  }, [id]);
+
+  // async function fetchData() {
+  //   console.log(`refetching stuff`);
+  //   // refetch();
+  //   const { data } = await client.query({ query: GetTweets });
+  //   console.log(`done fetching ${data}`);
+  //   setTweets(data.tweets);
+  // }
+
+  // fetchData();
+
+  const refresh = () => {
+    // navigate("/");
+    navigate(`/yay/${id ? parseInt(id) + 1 : 1}`);
+  };
+
+  return (
+    <div>
+      <button onClick={refresh}>reload data</button>
+      <pre>{JSON.stringify(tweets, null, 2)}</pre>
+    </div>
+  );
+}
+
 function App() {
-  const { loading, error, data } = useQuery(GetTweets, {
-    onError: (error) => {
-      console.log(`Apollo error: ${JSON.stringify(error, null, 2)}`);
-    },
-  });
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/yay/:id" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
